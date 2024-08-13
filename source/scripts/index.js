32b9dff39f2278267338ed0255238b7c0cc11a29
TatianaSenatorova <108460915+TatianaSenatorova@users.noreply.github.com>
2024-08-13 11:35:08 +0400

    Создайте базовую стилизацию с помощью препроцессора (#7)
    * Добавляет шрифты с помощью миксина и переменную $primary-font
    
    * Добавляет переменные
    
    * Добавляет бургер
    
    * Скорректирован лого
    
    * Перенесла переменные шрифтов в файл variables.scss
    
    * Добавляет бургер-меню
    
    * Корректирует header
    
    * Корректирует header
    
    * Добавляет часть hero блока
    
    * Стилизует hero и начало стилизации блока программы
    
    * Продолжение стилизации секции программы
    
    * Убирает ненужные миксины
    
    * Убирает ./ из путей к файлам
    
    * Меняет фавиконы и добавляет стилизацию секции programs
    
    * Закончена стилизация секции programs
    
    * Начало стилизаии блока application
    
    * Стилизует секцию application
    
    * Закончена стилизация секции application
    
    * Продолжение стилизации секции experience
    
    * Закончена стилизация секции experience
    
    * Стилизует секцию map
    
    * Переделала блок header и hero
    
    * Исправляет шрифты и стилизует секцию map
    
    * Закончена стилизация секции footer и map
    
    * Изменяет  список в блоке experience
    
    * Корректирует бургер-меню
    
    * Добавляет ссылки в меню каталога, кастомную переменную для текущего элемента навигации
    
    * Начало стилизации страницы каталога
    
    * Мобильная стилизация блока product-list
    
    * Стилизует карточки и заголовок секции additions
    
    * Закончена стилизация мобильной версии страницы каталога
    
    * Частично переделана стилизация мобильной версии каталога и начата стилизация планшетной версии
    
    * Убирает flex и  flex-direction: column где они не нужны
    
    * Коррекция слайдера
    
    * Корректирует секцию experience
    
    * Корректирует иконку-стрелку в секции programs
    
    * Корректирует фон слайдера
    
    * Корректирует карточку товара и заголовок с линией
    
    * Меняет появление меню на мобильной версии и корректирует слайдер
    
    * Корректирует карточки товаров
    
    * Доделана планшетная версия страницы каталога
    
    * Закончена адаптация под десктопную версию страницы каталога
    
    * Изменила фавиконки и убрала пустые строки после проверки линтером
    
    * Убрала кастомную переменную, так как выдавал ошибку линтер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,50 @@
 /* в этот файл добавляет скрипты*/
+
+//Открытие и закрытие меню
+const burgerEl = document.querySelector('.burger');
+const navListEl = document.querySelector('.nav-list');
+const bodyEl = document.querySelector('.page__body');
+
+burgerEl.addEventListener('click', () => {
+  burgerEl.classList.toggle('is-opened');
+  navListEl.classList.toggle('is-opened');
+  bodyEl.classList.toggle('no-scroll');
+});
+
+//Слайдер
+
+const sliderDividerEl = document.querySelector('.slider__divider');
+const sliderZoneEl = document.querySelector('.slider');
+const sliderBeforeEl = document.querySelector('.slider__image-before');
+
+let mouseEvent = false;
+
+sliderDividerEl.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  mouseEvent = true;
+});
+
+sliderDividerEl.addEventListener('mouseup', (e) => {
+  e.preventDefault();
+  mouseEvent = false;
+});
+
+sliderZoneEl.addEventListener('mouseup', () => {
+  mouseEvent = false;
+});
+
+sliderZoneEl.addEventListener('mousemove', (e) => {
+  const x = e.pageX - sliderZoneEl.offsetLeft;
+  let percent = x * 100 / sliderZoneEl.offsetWidth;
+
+  if (percent < 0) {
+    percent = 0;
+  } else if (percent > 100) {
+    percent = 100;
+  }
+
+  if (mouseEvent) {
+    sliderDividerEl.style.left = `${percent }%`;
+    sliderBeforeEl.style.width = `${percent }%`;
+  }
+});
